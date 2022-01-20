f5dfe269e3b6d13ab13fae7d5724b9ecb1c6e685
polarcube1 <polarcube1@gmail.com>
2022-01-20 17:09:42 -0500

    Added Sprites

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import pygame
+import os
+
+pygame.init()
+
+# variables
+CAR_SPEED = 10
+
+Y = 'y'
+
+screen = pygame.display.set_mode((512, 512))
+    # images
+PLAYER_IMG = pygame.image.load('imgs/car.png').convert() 
+TRACK_IMG = pygame.image.load('imgs/track.png').convert()
+
+objects = []
+
+screen.blit(TRACK_IMG, (0, 0))
+
+class Car:
+    def __init__(self, x, y):
+        self.speed = CAR_SPEED
+        self.velocity = 0
+        self.rotation = 0
+        self.img = PLAYER_IMG
+    
+    def move(self, speed, input):
+        
+        
+
+while 1:
+    for event in pygame.event.get():
+        if event.type in (pygame.QUIT, pygame.KEYDOWN):
+            sys.exit()
+    pygame.display.update()
+    pygame.time.delay(100)
